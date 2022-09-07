f444152c1f5f2f4dbed8be27d9d837bfc434a39c
Amy Gori <amydgori@gmail.com>
2022-09-07 09:16:37 -0400

    Add settings for connecting to postgres db

--- django_music/settings.py
+++ django_music/settings.py
@@ -86,12 +86,17 @@ WSGI_APPLICATION = "django_music.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "example-django-music",
+#         "USER": "example-django-music",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
+DATABASES = {"default": env.db()}
 
 
 # Password validation
